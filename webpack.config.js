5fee1fe4cc0a2c40b5f8b6a55bd7b4b4672dac65
vin <vincent.hilton@woodlanddirect.com>
2018-03-19 12:49:37 -0400

    mod webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,36 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var debug = process.argv.indexOf('-p') === -1
+var webpack = require('webpack')
 
 module.exports = {
-  context: path.resolve(__dirname, 'examples/src'),
+  context: __dirname,
+  devtool: debug ? 'inline-sourcemap' : false,
   entry: {
-    app: './app.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'examples/dist'),
-    filename: '[name].js',
-    publicPath: '/',
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'examples/src'),
-    host: '0.0.0.0',
-    port: 8000,
+    header: './src/Lightbox.js'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        use: [{
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
           loader: 'babel-loader',
-          options: { presets: ['react', 'es2015', 'stage-0'] },
-        }],
-      },
-      { 
-        test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'less-loader'],
-        })
-      },
-      {   
-        test: /\.html$/,
-        use: [{
-          loader: 'html-loader',
-        }]   
-      }, 
-    ],
+          options: {
+            presets: [['env', {
+              targets: {
+                browsers: ['last 3 major versions', 'IE >= 11']
+              }
+            }], 'react'],
+            plugins: ['transform-decorators-legacy', 'transform-class-properties']
+          }
+        }
+      }
+    ]
   },
   resolve: {
-    alias: {
-      'react-images': path.resolve(__dirname, 'src/Lightbox'),
-    }
+    extensions: ['.js', '.jsx']
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common',
-      filename: 'common.js',
-      minChunk: 2,
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      inject: false,
-      template: path.resolve(__dirname, 'examples/src/index.html')
-    }),
-    new ExtractTextPlugin('example.css'),
-  ]
-};
+  output: {
+    path: __dirname + '/lib',
+    filename: '[name].js'
+  }
+}
